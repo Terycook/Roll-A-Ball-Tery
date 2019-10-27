ecf0700afa4e9fe38cf2869b0abe057abb15b4d2
Tery Cook <cook48@nsuok.edu>
2019-10-26 20:03:03 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text scoreText;
+    public Text winText;
+    public static string endScore;
+    
+    
+
+    private Rigidbody rb;
+    private int score;
+    
+    
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        winText.text = "";
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            SetScoreText();
+            endScore = score.ToString();
+            
+        }
+    }
+    void SetScoreText()
+    {
+
+        AudioSource audio = gameObject.AddComponent<AudioSource>();
+            AudioClip clip = (AudioClip)Resources.Load("youwin");
+
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 14)
+        {
+            winText.text = "You Win!!!";
+            
+            audio.PlayOneShot(clip);
+        }
+            
+    }
+}
---
+++ Assets/Scripts/ShowData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// this script is attached to the camera. It executes when the camera awakes and loads the values in the text boxes on the panel so you don't have to click a button.
+
+public class ShowData : MonoBehaviour
+{
+    public Text NameText;
+    public Text RoundsText;
+    public Text ScoreText;
+    public Text BallSpeedText;
+    
+    
+
+    private void Awake()
+    {
+        NameText.text = "Name: " + KeepData.PlayerName;
+        RoundsText.text = "Lives: " + KeepData.PlayerRounds.ToString();
+        BallSpeedText.text = "Speed: " + KeepData.PlayingSpeed.ToString();
+        ScoreText.text = "Score: " + PlayerController.endScore;
+        
+
+    }
+
+}
